b95d05747a2afdab50f99a5c77c3f561f917bf8b
magicdeveloper250 <impanodeveloper@proton.me>
2025-08-29 11:07:25 +0200

    hosting

--- ExamManagementSystem/settings.py
+++ ExamManagementSystem/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
        'rest_framework_simplejwt',
        'rest_framework_simplejwt.token_blacklist',
        'corsheaders',
-      "Users",
+      "users",
       "student",
       "courses",
       "rooms",
