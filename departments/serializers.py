cf81675cf14530cf31e7ab28553acf0e3dc19ad5
magicdeveloper250 <impanodeveloper@proton.me>
2025-09-10 10:40:42 +0200

    changing slot logic

--- departments/serializers.py
+++ departments/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
+
+from rooms.serializers import LocationSerializer
 from .models import Department
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 class DepartmentSerializer(serializers.ModelSerializer):
+    location= LocationSerializer(read_only=True)
     class Meta:
         model = Department
-        fields = '__all__'
+        fields =  ['id', 'code', 'name', 'location']
